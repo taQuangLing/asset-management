4e910a44fc83772150be49ca3d77cc22581d69d2
taQuangLing <linh.tq194605@gmail.com>
2023-04-23 14:19:12 +0700

    login

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<form action="action_page.php" method="post">
+    <div class="container">
+        <label for="uname"><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required><br>
+
+        <label for="psw"><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required><br>
+
+        <button type="submit">Login</button>
+    </div>
+</form>
