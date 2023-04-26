850ff045155edac00d9015b5a05eb065bdc3148c
HungLV <viethung9022@gmail.com>
2023-04-26 18:30:29 +0700

    add function CRUD

--- app/Http/Controllers/AssetDetailController.php
+++ app/Http/Controllers/AssetDetailController.php
@@ -15,43 +15,63 @@ use PDO;
 
 class AssetDetailController extends Controller
 {
-    public function index(): Factory|View|Application
-    {
-        $data = DB::table('asset_detail')
-            ->join('assets', 'asset_detail.ma_tai_san', '=', 'assets.ma_tai_san')
-            ->join('coupons', 'asset_detail.ma_phieu_nhap', '=', 'coupons.ma_phieu_nhap')
-            ->join('providers', 'assets.ma_ncc', '=', 'providers.ma_ncc')
-            ->select('asset_detail.ma_tai_san as Mã tài sản', 'asset_detail.ma_phieu_nhap as mã phiếu nhập',
-                'assets.ten_tai_san as Tên tài sản', 'assets.loai_tai_san as Loại tài sản ',
-                'asset_detail.don_gia as Đơn giá (VND)', 'asset_detail.so_luong as Số lượng',
-                'assets.han_bao_hanh as Hạn bảo hành', 'providers.ten_ncc as Tên NCC')
-            ->groupBy('asset_detail.ma_tai_san', 'asset_detail.ma_phieu_nhap', 'asset_detail.don_gia','asset_detail.so_luong')
-            ->distinct()
-            ->get();
-        return view('layout', ['data' => $data]);
+  public function index(): Factory|View|Application
+  {
+    $data = DB::table('asset_detail')
+      ->join('assets', 'asset_detail.ma_tai_san', '=', 'assets.ma_tai_san')
+      ->join('coupons', 'asset_detail.ma_phieu_nhap', '=', 'coupons.ma_phieu_nhap')
+      ->join('providers', 'assets.ma_ncc', '=', 'providers.ma_ncc')
+      ->select('asset_detail.ma_tai_san as Mã tài sản', 'asset_detail.ma_phieu_nhap as mã phiếu nhập',
+        'assets.ten_tai_san as Tên tài sản', 'assets.loai_tai_san as Loại tài sản ',
+        'asset_detail.don_gia as Đơn giá (VND)', 'asset_detail.so_luong as Số lượng',
+        'assets.han_bao_hanh as Hạn bảo hành', 'providers.ten_ncc as Tên NCC')
+      ->groupBy('asset_detail.ma_tai_san', 'asset_detail.ma_phieu_nhap', 'asset_detail.don_gia', 'asset_detail.so_luong')
+      ->distinct()
+      ->get();
+    return view('layout', ['data' => $data]);
+  }
+
+  public function asset_detail($ma_tai_san): JsonResponse
+  {
+    try {
+      $data = DB::table('asset_detail')
+        ->join('assets', 'asset_detail.ma_tai_san', '=', 'assets.ma_tai_san')
+        ->join('coupons', 'asset_detail.ma_phieu_nhap', '=', 'coupons.ma_phieu_nhap')
+        ->join('providers', 'providers.ma_ncc', '=', 'assets.ma_ncc')
+        ->join('guarantee_info', 'asset_detail.ma_tai_san', '=', 'guarantee_info.ma_tai_san')
+        ->join('liquidation_list', 'asset_detail.ma_tai_san', '=', 'liquidation_list.ma_tai_san')
+        ->select('asset_detail.ma_tai_san', 'asset_detail.ma_phieu_nhap', 'assets.ten_tai_san',
+          'assets.loai_tai_san', 'asset_detail.don_gia', 'assets.han_bao_hanh', 'assets.hang_san_xuat',
+          'providers.ten_ncc', 'providers.dia_chi_ncc', 'providers.sdt_ncc', 'providers.ma_ncc',
+          'coupons.ngay_nhap', 'assets.mo_ta', 'guarantee_info.trang_thai as tinh_trang', 'liquidation_list.trang_thai')
+        ->where('asset_detail.ma_tai_san', '=', $ma_tai_san)
+        ->limit(1)
+        ->get();
+      return response()->json($data);
+    } catch (\Exception $e) {
+      echo $e;
     }
+  }
 
-    public function asset_detail(Request $request, $ma_tai_san): JsonResponse
-    {
-        try {
-            $data = DB::table('asset_detail')
-                ->join('assets', 'asset_detail.ma_tai_san', '=', 'assets.ma_tai_san')
-                ->join('coupons', 'asset_detail.ma_phieu_nhap', '=', 'coupons.ma_phieu_nhap')
-                ->join('providers', 'providers.ma_ncc', '=', 'assets.ma_ncc')
-                ->join('guarantee_info', 'asset_detail.ma_tai_san', '=', 'guarantee_info.ma_tai_san')
-                ->join('liquidation_list', 'asset_detail.ma_tai_san', '=', 'liquidation_list.ma_tai_san')
-                ->select('asset_detail.ma_tai_san', 'asset_detail.ma_phieu_nhap', 'assets.ten_tai_san',
-                    'assets.loai_tai_san', 'asset_detail.don_gia', 'assets.han_bao_hanh', 'assets.hang_san_xuat',
-                    'providers.ten_ncc', 'providers.dia_chi_ncc', 'providers.sdt_ncc', 'providers.ma_ncc',
-                    'coupons.ngay_nhap', 'assets.mo_ta', 'guarantee_info.trang_thai as tinh_trang', 'liquidation_list.trang_thai')
-                ->where('asset_detail.ma_tai_san', '=', $ma_tai_san)
-                ->limit(1)
-                ->get();
-            return response()->json($data);
-        } catch (\Exception $e) {
-            echo $e;
-        }
+  public function search($ma_tai_san, $ma_phieu_nhap, $loai_tai_san, $ncc, $start_date, $end_date, $han_bao_hanh)
+  {
 
+  }
 
-    }
+  public function searchByName($ten_tai_san)
+  {
+
+  }
+
+  public function updateAsset(Request $request)
+  {
+//    $name = $request->input('name');
+//    $email = $request->input('email');
+  }
+
+  public function deleteAsset($ma_tai_san)
+  {
+
+  }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
@@ -27,26 +27,18 @@ Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/quanlytaisan', [AssetDetailController::class, 'index'])->name('quanlytaisan');
 
 Route::get('/phieunhap', function () {
-    return view('quanlyphieunhap');
+  return view('quanlyphieunhap');
 })->name('phieunhap');
 
 Route::get('/get-asset-detail', [AssetDetailController::class, 'index']);
 Route::get('/fetch-data-asset/{ma_tai_san}', [AssetDetailController::class, 'asset_detail']);
 
-//Route::get('/fetch-data-asset/{ma_tai_san}', function ($ma_tai_san) {
-//    $data = DB::table('asset_detail')
-//        ->join('assets', 'asset_detail.ma_tai_san', '=', 'assets.ma_tai_san')
-//        ->join('coupons', 'asset_detail.ma_phieu_nhap', '=', 'coupons.ma_phieu_nhap')
-//        ->join('providers', 'assets.ma_ncc', '=', 'providers.ma_ncc')
-//        ->join('guarantee_info', 'asset_detail.ma_tai_san', '=', 'guarantee_info.ma_tai_san')
-//        ->join('liquidation_list', 'asset_detail.ma_tai_san', '=', 'liquidation_list.ma_tai_san')
-//        ->select('asset_detail.ma_tai_san', 'asset_detail.ma_phieu_nhap', 'assets.ten_tai_san',
-//            'assets.loai_tai_san', 'asset_detail.don_gia', 'assets.han_bao_hanh', 'assets.hang_san_xuat',
-//            'providers.ten_ncc', 'providers.dia_chi', 'providers.sdt', 'providers.ma_ncc',
-//            'coupons.ngay_nhap', 'assets.mo_ta', 'guarantee_info.trang_thai', 'liquidation_list.trnag_thai')
-//        ->where('asset_detail.ma_tai_san', '=', $ma_tai_san)
-//        ->get();
-//    return response()->json($data);
-//});
+Route::get('/search?ma_tai_san={ma_tai_san}?ma_phieu_nhap={ma_phieu_nhap}?
+loai_tai_san={loai_tai_san}?ncc={ncc}?start_date={from_date}?end_date={end_date}?han_bao_hanh={han_bao_hanh}',
+  [AssetDetailController::class, 'search']);
 
+Route::get('/searchByName/{ten_tai_san}', [AssetDetailController::class, 'searchByName'])->name('searchByName');
 
+Route::post('/updateAsset', [AssetDetailController::class, 'updateAsset'])->name('updateAsset');
+
+Route::delete('/deleteAsset/{ma_tai_san}', [AssetDetailController::class, 'deleteAsset'])->name('deleteAsset');
